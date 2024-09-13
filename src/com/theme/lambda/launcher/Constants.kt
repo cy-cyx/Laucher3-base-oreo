32da979d333e2e64dc5f36a175bdc7df466c2743
haianhsing <xinghaian@uniteman.com>
2024-09-13 11:18:05 +0800

    init config

---
+++ src/com/theme/lambda/launcher/Constants.kt
@@ -0,0 +1,8 @@
+package com.theme.lambda.launcher
+
+object Constants {
+    const val BASE_URL = "https://d218bwdbonjwkx.cloudfront.net/"
+    const val SECRET_KEY = "3b3ffe856169d39c"
+    const val PRIVACY_POLICY = "https://d218bwdbonjwkx.cloudfront.net/privacy-policy"
+    const val TERMS_OF_SERVICE = "https://d218bwdbonjwkx.cloudfront.net/terms-of-service"
+}
