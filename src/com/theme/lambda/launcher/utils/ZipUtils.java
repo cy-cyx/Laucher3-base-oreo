1bdaaa3431e11462a22966aea67abb5a5f261832
caiyixiang <caiyixiang@uniteman.com>
2024-09-28 19:49:44 +0800

    修改zip安全漏洞

--- src/com/theme/lambda/launcher/utils/ZipUtils.java
+++ src/com/theme/lambda/launcher/utils/ZipUtils.java
@@ -37,44 +37,21 @@ public class ZipUtils {
         String szName = "";
         while ((zipEntry = inZip.getNextEntry()) != null) {
             szName = zipEntry.getName();
-            if (zipEntry.isDirectory()) {
-                //获取部件的文件夹名
-                szName = szName.substring(0, szName.length() - 1);
-                File folder = new File(outPathString + File.separator + szName);
-                folder.mkdirs();
-            } else {
-                Log.e(TAG, outPathString + File.separator + szName);
-                File file = new File(outPathString + File.separator + szName);
-                if (!file.exists()) {
-                    Log.e(TAG, "Create the file:" + outPathString + File.separator + szName);
-                    file.getParentFile().mkdirs();
-                    file.createNewFile();
-                }
-                // 获取文件的输出流
-                FileOutputStream out = new FileOutputStream(file);
-                int len;
-                byte[] buffer = new byte[4096];
-                // 读取（字节）字节到缓冲区
-                while ((len = inZip.read(buffer)) != -1) {
-                    // 从缓冲区（0）位置写入（字节）字节
-                    out.write(buffer, 0, len);
-                    out.flush();
-                }
-                out.close();
-            }
-        }
-        inZip.close();
-    }
 
-    public static void UnZipFolder(String zipFileString, String outPathString, String szName) throws Exception {
-        ZipInputStream inZip = new ZipInputStream(new FileInputStream(zipFileString));
-        ZipEntry zipEntry;
-        while ((zipEntry = inZip.getNextEntry()) != null) {
-            //szName = zipEntry.getName();
+            if(szName.contains("../")){
+                continue;
+            }
+
             if (zipEntry.isDirectory()) {
                 //获取部件的文件夹名
                 szName = szName.substring(0, szName.length() - 1);
                 File folder = new File(outPathString + File.separator + szName);
+
+                String canonicalPath = folder.getCanonicalPath();
+                if (!canonicalPath.startsWith(outPathString)) {
+                    continue;
+                }
+
                 folder.mkdirs();
             } else {
                 Log.e(TAG, outPathString + File.separator + szName);
@@ -84,6 +61,12 @@ public class ZipUtils {
                     file.getParentFile().mkdirs();
                     file.createNewFile();
                 }
+
+                String canonicalPath = file.getCanonicalPath();
+                if (!canonicalPath.startsWith(outPathString)) {
+                    continue;
+                }
+
                 // 获取文件的输出流
                 FileOutputStream out = new FileOutputStream(file);
                 int len;
