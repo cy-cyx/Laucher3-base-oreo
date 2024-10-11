9a3cb520368788fb6924264c044ddafaff6d46bf
caiyixiang <caiyixiang@uniteman.com>
2024-10-11 14:13:33 +0800

    修复webview多进程问题

--- src/com/theme/lambda/launcher/App.kt
+++ src/com/theme/lambda/launcher/App.kt
@@ -2,11 +2,16 @@ package com.theme.lambda.launcher
 
 import android.app.Application
 import android.content.Context
+import android.os.Build
+import android.os.Looper
+import android.text.TextUtils
+import android.webkit.WebView
 import com.lambdaweather.LambdaWeather
 import com.theme.lambda.launcher.data.di.allModules
 import com.theme.lambda.launcher.statistics.EventUtil
 import com.theme.lambda.launcher.statistics.FirebaseAnalyticsUtil
 import com.theme.lambda.launcher.utils.CommonUtil
+import com.theme.lambda.launcher.utils.OsUtil
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
@@ -15,10 +20,23 @@ class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        initKoin()
-        LambdaWeather.init(this, Constants.BASE_URL)
-        FirebaseAnalyticsUtil.init(this)
-        EventUtil.init()
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            val process = getProcessName()
+            if (packageName != process) WebView.setDataDirectorySuffix(process)
+        }
+        val processName: String? = OsUtil.getCurProcessName(this)
+        if (!TextUtils.isEmpty(processName)) {
+            val defaultProcess = processName == packageName
+            if (defaultProcess) {
+                initKoin()
+                LambdaWeather.init(this, Constants.BASE_URL)
+                FirebaseAnalyticsUtil.init(this)
+                EventUtil.init()
+            }
+        }
+
+
     }
 
     override fun attachBaseContext(base: Context?) {
---
+++ src/com/theme/lambda/launcher/utils/OsUtil.kt
@@ -0,0 +1,35 @@
+package com.theme.lambda.launcher.utils
+
+import android.app.ActivityManager
+import android.content.Context
+import android.os.Process
+
+
+object OsUtil {
+    fun getProcessName(cxt: Context, pid: Int): String? {
+        //获取ActivityManager对象
+        val am: ActivityManager = cxt.getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
+        //在运行的进程的
+        val runningApps: List<ActivityManager.RunningAppProcessInfo> = am.getRunningAppProcesses()
+            ?: return null
+        for (procInfo in runningApps) {
+            if (procInfo.pid == pid) {
+                return procInfo.processName
+            }
+        }
+        return null
+    }
+
+    fun getCurProcessName(context: Context): String? {
+        val pid = Process.myPid()
+        val activityManager = context.getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
+        for (appProcess in activityManager
+            .runningAppProcesses) {
+            if (appProcess.pid == pid) {
+                return appProcess.processName
+            }
+        }
+        return null
+    }
+
+}
