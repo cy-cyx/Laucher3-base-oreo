929e75b93c1659d649023f77c56d594c6b6599c8
caiyixiang <caiyixiang@uniteman.com>
2024-10-22 17:07:42 +0800

    banner同步充电方案

--- src/com/theme/lambda/launcher/ad/view/AdBanner2.kt
+++ src/com/theme/lambda/launcher/ad/view/AdBanner2.kt
@@ -4,56 +4,53 @@ import android.app.Activity
 import android.content.Context
 import android.util.AttributeSet
 import android.widget.FrameLayout
-import android.widget.ImageView
 import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
 import androidx.lifecycle.LifecycleOwner
-import com.airbnb.lottie.LottieAnimationView
-import com.android.launcher3.R
 import com.lambda.adlib.LambdaAd
 import com.lambda.adlib.LambdaAdAdapter
 import com.lambda.adlib.adapter.LAdMultipleAdapter
-import com.theme.lambda.launcher.utils.gone
-import com.theme.lambda.launcher.utils.visible
-import com.theme.lambda.launcher.vip.VipManager
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
-// 支持折叠banner且仅切换时刷新（不然折叠banner一直挡住使用）
-class ADBanner @JvmOverloads constructor(
+class AdBanner2 @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null
 ) : FrameLayout(context, attrs), LifecycleEventObserver {
-
-    private val lottie = LottieAnimationView(context)
+    private var job: Job? = null
+    private val scope = CoroutineScope(SupervisorJob() + Dispatchers.Main)
+    private var isResume = true
+    private var isLoadedCollapse = false
+    var scenesName: String = ""
+    val autoRefreshInterval: Long = 30 * 1000L
 
     init {
         (context as? FragmentActivity)?.lifecycle?.addObserver(this)
-
-        addView(lottie)
-        lottie.loop(true)
-        lottie.scaleType = ImageView.ScaleType.FIT_XY
-        lottie.setAnimation(R.raw.banner_loading)
     }
-
-    var scenesName = ""
 
     override fun onStateChanged(source: LifecycleOwner, event: Lifecycle.Event) {
         when (event) {
             Lifecycle.Event.ON_RESUME -> {
-                if (VipManager.isVip.value == false) {
-                    showBannerAd()
-                }
+                isResume = true
+                showBannerAd()
+            }
+
+            Lifecycle.Event.ON_PAUSE -> {
+                isResume = false
             }
 
             Lifecycle.Event.ON_DESTROY -> {
+                job?.cancel()
                 mBannerAdapter?.destroy()
             }
 
             else -> {}
         }
     }
-
-    private var lastShowAdTimeStamp = -1L
-    private var bannerLoadCnt = 0
 
     private var mBannerAdapter: LAdMultipleAdapter? = null
     private fun showBannerAd() {
@@ -65,24 +62,40 @@ class ADBanner @JvmOverloads constructor(
                     override fun onLoad(adapter: LAdMultipleAdapter, status: Int) {
                         super.onLoad(adapter, status)
                         if (status == LambdaAd.AD_FILL) {
-                            // sdk里面可能有个源同时更新，刷新限制避免刷新
-                            if (System.currentTimeMillis() - lastShowAdTimeStamp < 5000) return
-                            adapter.showBanner(this@ADBanner, isLoadShow = false)
-                            lastShowAdTimeStamp = System.currentTimeMillis()
-
+                            if (!isResume) return
+                            adapter.showBanner(this@AdBanner2, isLoadShow = false)
+                            isLoadedCollapse = true
+                            showAutoShow(autoRefreshInterval)
                         } else if (status == LambdaAd.AD_LOAD_FAIL) {
-                            lottie.visible()
                         }
                     }
 
                     override fun onClose(adapter: LAdMultipleAdapter, status: Int) {
                         super.onClose(adapter, status)
                         if (status == LambdaAd.AD_SHOWING) {
-                            lottie.gone()
+
+                        } else if (status == LambdaAd.AD_CLICK) {
+
                         }
                     }
                 })
         }
-        mBannerAdapter?.loadBanner(isAuto = false, tryCollapse = bannerLoadCnt++ % 5 == 0)
+        mBannerAdapter?.loadBanner(false, !isLoadedCollapse)
+    }
+
+    private fun showAutoShow(delay: Long) {
+        job?.cancel()
+        job = scope.launch {
+            delay(delay)
+            if (!isResume) {
+                return@launch
+            }
+            if (mBannerAdapter?.isReady() == true) {
+                showBannerAd()
+            } else {
+                // 如果没有准备好 5秒后再试试
+                showAutoShow(5000)
+            }
+        }
     }
 }
