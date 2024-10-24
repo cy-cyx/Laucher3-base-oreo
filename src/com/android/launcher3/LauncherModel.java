0e9a3f3a85e776f42bbd16d11bc5b22053678aab
caiyixiang <caiyixiang@uniteman.com>
2024-10-24 19:23:15 +0800

    暂时不崩溃把 后续再解决

--- src/com/android/launcher3/LauncherModel.java
+++ src/com/android/launcher3/LauncherModel.java
@@ -347,15 +347,16 @@ public class LauncherModel extends BroadcastReceiver
             // the modelItem needs to match up perfectly with item if our model is
             // to be consistent with the database-- for now, just require
             // modelItem == item or the equality check above
-            String msg = "item: " + ((item != null) ? item.toString() : "null") +
-                    "modelItem: " +
-                    ((modelItem != null) ? modelItem.toString() : "null") +
-                    "Error: ItemInfo passed to checkItemInfo doesn't match original";
-            RuntimeException e = new RuntimeException(msg);
-            if (stackTrace != null) {
-                e.setStackTrace(stackTrace);
-            }
-            throw e;
+// todo 暂时不崩溃
+//            String msg = "item: " + ((item != null) ? item.toString() : "null") +
+//                    "modelItem: " +
+//                    ((modelItem != null) ? modelItem.toString() : "null") +
+//                    "Error: ItemInfo passed to checkItemInfo doesn't match original";
+//            RuntimeException e = new RuntimeException(msg);
+//            if (stackTrace != null) {
+//                e.setStackTrace(stackTrace);
+//            }
+//            throw e;
         }
     }
 
