71e01e2ce01bd413aebe14c539db3328b4c690cd
Tower Lin <tower.lin@uniteman.com>
2024-11-08 20:50:35 +0800

    增加iOS分类

--- src/com/theme/lambda/launcher/Constants.kt
+++ src/com/theme/lambda/launcher/Constants.kt
@@ -10,7 +10,7 @@ object Constants {
     const val adjustToken = "zsckz2n5xcsg"
 
     val sThemeTag =
-        arrayListOf("Hot", "Glittery", "Anime", "Aesthetic", "Cool", "Pink", "Color", "Neon")
+        arrayListOf("Hot", "Glittery", "iOS", "Anime", "Aesthetic", "Cool", "Pink", "Color", "Neon")
 
     @JvmField
     val sAllppAction = "ALL_APPS"
