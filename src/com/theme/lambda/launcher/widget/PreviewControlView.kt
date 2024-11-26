04f1a4d57705c444a6204d8066725acb7449f1f9
caiyixiang <caiyixiang@uniteman.com>
2024-11-26 18:07:06 +0800

    设置增加调整入口

--- src/com/theme/lambda/launcher/widget/PreviewControlView.kt
+++ src/com/theme/lambda/launcher/widget/PreviewControlView.kt
@@ -10,6 +10,8 @@ import com.android.launcher3.ThemeManager
 import com.android.launcher3.databinding.LayoutPreviewControlBinding
 import com.theme.lambda.launcher.statistics.EventName
 import com.theme.lambda.launcher.statistics.EventUtil
+import com.theme.lambda.launcher.ui.effect.EffectActivity
+import com.theme.lambda.launcher.ui.layoutadjust.LayoutAdjustActivity
 import com.theme.lambda.launcher.utils.LauncherUtil
 import com.theme.lambda.launcher.utils.SystemUtil
 import com.theme.lambda.launcher.utils.gone
@@ -40,6 +42,12 @@ class PreviewControlView @JvmOverloads constructor(
         layoutPreviewControlBinding?.setIconTv?.setOnClickListener {
             controlListen?.setIcon()
         }
+        layoutPreviewControlBinding?.setLayoutIv?.setOnClickListener {
+            EffectActivity.start(context)
+        }
+        layoutPreviewControlBinding?.setLayoutIv?.setOnClickListener {
+            LayoutAdjustActivity.start(context)
+        }
 
         // 处理兼容性问题 已经知道华为安卓10以上
         if (!LauncherUtil.isDefaultLauncher(context)
@@ -47,14 +55,14 @@ class PreviewControlView @JvmOverloads constructor(
                     || SystemUtil.getDeviceBrand().equals(SystemUtil.PHONE_HONOR))
             && Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q
         ) {
-            layoutPreviewControlBinding?.setTv?.gone()
-            layoutPreviewControlBinding?.setLl?.visible()
+            layoutPreviewControlBinding?.setLl?.gone()
+            layoutPreviewControlBinding?.setNoApplyLl?.visible()
             EventUtil.logEvent(EventName.setIconEnable, Bundle().apply {
                 putString("id", ThemeManager.getThemeManagerIfExist()?.previewThemeId)
             })
         } else {
-            layoutPreviewControlBinding?.setTv?.visible()
-            layoutPreviewControlBinding?.setLl?.gone()
+            layoutPreviewControlBinding?.setLl?.visible()
+            layoutPreviewControlBinding?.setNoApplyLl?.gone()
         }
 
     }
