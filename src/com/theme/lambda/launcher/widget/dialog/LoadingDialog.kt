0517f802dc459fbc9a48d9be1fb371051f12c6af
Tower Lin <tower.lin@uniteman.com>
2024-11-26 22:24:04 +0800

    ui调整

--- src/com/theme/lambda/launcher/widget/dialog/LoadingDialog.kt
+++ src/com/theme/lambda/launcher/widget/dialog/LoadingDialog.kt
@@ -5,12 +5,12 @@ import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import com.android.launcher3.R
-import com.android.launcher3.databinding.DialogLaodingBinding
+import com.android.launcher3.databinding.DialogLoadingBinding
 import com.theme.lambda.launcher.utils.CommonUtil
 
 class LoadingDialog(context: Context) : Dialog(context, R.style.Theme_translucentDialog) {
 
-    val viewBinding = DialogLaodingBinding.inflate(LayoutInflater.from(context))
+    val viewBinding = DialogLoadingBinding.inflate(LayoutInflater.from(context))
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
