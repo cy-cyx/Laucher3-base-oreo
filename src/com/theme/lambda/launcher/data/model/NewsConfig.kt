8399b175327b943e6f2a2cbc16f70cfa729bbc07
Tower Lin <tower.lin@uniteman.com>
2024-12-04 21:54:54 +0800

    news支持远程配置控制mrec行为

---
+++ src/com/theme/lambda/launcher/data/model/NewsConfig.kt
@@ -0,0 +1,17 @@
+package com.theme.lambda.launcher.data.model
+
+import com.google.gson.annotations.SerializedName
+
+class NewsConfig {
+    @SerializedName("enable_detail_top_ad")
+    var enableDetailTopAd = true
+
+    @SerializedName("enable_detail_bottom_ad")
+    var enableDetailBottomAd = true
+
+    @SerializedName("detail_ad_interval")
+    var detailAdInterval = 10
+
+    @SerializedName("list_ad_interval")
+    var listAdInterval = 5
+}
